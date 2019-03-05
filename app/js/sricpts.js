7650ed49400c8cbb4d0c52c7a6d6cb06638665d6
folkaras <efolkaras@gmail.com>
2019-03-05 22:23:40 +0300

    initial commit

---
+++ app/js/sricpts.js
@@ -0,0 +1,6 @@
+var burger = document.querySelector('.burger-button__button');
+var mainMenu = document.querySelector('.site-nav');
+
+burger.addEventListener("click", function(){
+	mainMenu.classList.toggle('site-nav--deployed');
+});
