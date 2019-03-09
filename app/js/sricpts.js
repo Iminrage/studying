1015a8d1e3c1a2794df7ba703c6a06fe4f7c98dd
folkaras <efolkaras@gmail.com>
2019-03-09 14:09:02 +0300

    Переверстал таблицы в одну в соответствии с макетом

--- app/js/sricpts.js
+++ app/js/sricpts.js
@@ -1,5 +1,15 @@
 var burger = document.querySelector('.burger-button__button');
 var mainMenu = document.querySelector('.site-nav');
+var scrollSlider = document.querySelector('.slider-overflow')
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+	var xwidth = (scrollSlider.scrollWidth/3) - (scrollSlider.scrollWidth/30);
+	console.log(xwidth);
+	scrollSlider.scrollTo(xwidth, 0);
+}, false);
 
 burger.addEventListener("click", function(){
 	mainMenu.classList.toggle('site-nav--deployed');
