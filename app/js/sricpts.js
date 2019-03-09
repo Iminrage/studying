847895e926dd56bb0b5afa90b6d58e3fd4e6ffe4
folkaras <efolkaras@gmail.com>
2019-03-09 14:54:41 +0300

    заскриптовал табличный слайдер

--- app/js/sricpts.js
+++ app/js/sricpts.js
@@ -11,6 +11,33 @@ document.addEventListener('DOMContentLoaded', function() {
 	scrollSlider.scrollTo(xwidth, 0);
 }, false);
 
+document.querySelector('#prices-dot1').addEventListener('click', function(){
+	var xwidth = (scrollSlider.scrollWidth/3) - (scrollSlider.scrollWidth/3);
+	console.log(xwidth);
+	scrollSlider.scrollTo(xwidth, 0);
+	document.querySelector('#prices-dot1').classList.add('slider-dots__dot--current');
+	document.querySelector('#prices-dot2').classList.remove('slider-dots__dot--current');
+	document.querySelector('#prices-dot3').classList.remove('slider-dots__dot--current');
+});
+document.querySelector('#prices-dot3').addEventListener('click', function(){
+	var xwidth = (scrollSlider.scrollWidth);
+	console.log(xwidth);
+	scrollSlider.scrollTo(xwidth, 0);
+	document.querySelector('#prices-dot3').classList.add('slider-dots__dot--current');
+	document.querySelector('#prices-dot1').classList.remove('slider-dots__dot--current');
+	document.querySelector('#prices-dot2').classList.remove('slider-dots__dot--current');
+});
+document.querySelector('#prices-dot2').addEventListener('click', function(){
+	var xwidth = (scrollSlider.scrollWidth/3) - (scrollSlider.scrollWidth/30);
+	console.log(xwidth);
+	scrollSlider.scrollTo(xwidth, 0);
+	document.querySelector('#prices-dot2').classList.add('slider-dots__dot--current');
+	document.querySelector('#prices-dot1').classList.remove('slider-dots__dot--current');
+	document.querySelector('#prices-dot3').classList.remove('slider-dots__dot--current');
+});
+
+
+
 burger.addEventListener("click", function(){
 	mainMenu.classList.toggle('site-nav--deployed');
 	if(mainMenu.classList.contains('site-nav--deployed')){
